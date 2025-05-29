d659725001d2a77f85c060c7ca1eae521f09ad8d
DanzoLuvsKFC <2459074@students.wits.ac.za>
2025-05-29 18:45:10 +0200

    Fixed up the navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import Courts from './pages/Courts';
 import CourtDetails from './pages/CourtDetails';
 import Bookings from './pages/Bookings';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/courts" element={<Courts />} />
         <Route path="/courts/:id" element={<CourtDetails />} />
         <Route path="/bookings" element={<Bookings />} />
+        <Route path="/login" element={<Login />} />      F
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,9 @@ export default function Navbar() {
     <nav style={{ padding: '1rem', backgroundColor: '#eee' }}>
       <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
       <Link to="/courts" style={{ marginRight: '1rem' }}>Courts</Link>
-      <Link to="/bookings">Bookings</Link>
+      <Link to="/bookings" style={{ marginRight: '1rem' }}>Bookings</Link>
+      <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
+      <Link to="/profile" style={{ marginRight: '1rem' }}>Profile</Link>
     </nav>
   );
 }
