2b9ab5de1d008221cd68fbacdbf689beb04f48d1
DanzoLuvsKFC <2459074@students.wits.ac.za>
2025-05-29 20:08:44 +0200

    Updated the courst with more mock data

--- src/data/courts.js
+++ src/data/courts.js
@@ -3,7 +3,7 @@ const courts = [
     id: '1',
     name: 'Braamfontein Fives',
     location: 'Johannesburg',
-    image: 'https://via.placeholder.com/200', // or use a placeholder image
+    image: 'https://picsum.photos/100', // or use a placeholder image
     price: 300,
     rating: 4.5,
     reviews: 20,
@@ -13,10 +13,30 @@ const courts = [
     id: '2',
     name: 'Pretoria Turf Arena',
     location: 'Pretoria',
-    image: 'https://via.placeholder.com/300',
+    image: 'https://picsum.photos/200',
     price: 250,
     rating: 4.2,
     reviews: 14,
+    availableSlots: ['17:00', '18:30', '20:00']
+  },
+  {
+    id: '3',
+    name: 'Melrose Madness',
+    location: 'Melrose',
+    image: 'https://picsum.photos/300',
+    price: 400,
+    rating: 5.0,
+    reviews: 99,
+    availableSlots: ['17:00', '18:30', '20:00']
+  },
+  {
+    id: '4',
+    name: 'Greenside Courts',
+    location: 'Greenside',
+    image: 'https://picsum.photos/600',
+    price: 200,
+    rating: 4.7,
+    reviews: 67,
     availableSlots: ['17:00', '18:30', '20:00']
   }
 ];
