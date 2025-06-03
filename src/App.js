a830d9f3a1dfa3be872d22d82895b83745f25a2f
DanzoLuvsKFC <2459074@students.wits.ac.za>
2025-06-03 20:38:36 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer'; // <-- Add this
+import Footer from './components/Footer'; 
 import Home from './pages/Home';
 import Courts from './pages/Courts';
 import CourtDetails from './pages/CourtDetails';
