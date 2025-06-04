8415cdbc97edf0c69230dbd0252c8e91d5740e46
DanzoLuvsKFC <2459074@students.wits.ac.za>
2025-06-04 16:47:03 +0200

    Again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Daniel Lasco. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Mark Daniel Lasco. All rights reserved.</p>
     </footer>
   );
 }
